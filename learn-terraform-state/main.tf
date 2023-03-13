989d3803e0e4a4543aef45a945eb5624ae72fb2c
gustjd887 <gustjd887@gmail.com>
2023-03-13 16:31:36 +0900

    learn-terraform-state

--- learn-terraform-state/main.tf
+++ learn-terraform-state/main.tf
@@ -32,7 +32,24 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "example" {
+# resource "aws_instance" "example" {
+#   ami                    = data.aws_ami.ubuntu.id
+#   instance_type          = "t2.micro"
+#   vpc_security_group_ids = [aws_security_group.sg_8080.id]
+#   user_data              = <<-EOF
+#               #!/bin/bash
+#               apt-get update
+#               apt-get install -y apache2
+#               sed -i -e 's/80/8080/' /etc/apache2/ports.conf
+#               echo "Hello World" > /var/www/html/index.html
+#               systemctl restart apache2
+#               EOF
+#   tags = {
+#     Name = "terraform-learn-state-ec2"
+#   }
+# }
+
+resource "aws_instance" "example_new" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.sg_8080.id]
@@ -66,15 +83,16 @@ resource "aws_security_group" "sg_8080" {
   }
 }
 
-output "instance_id" {
-  value = aws_instance.example.id
-}
+# output "instance_id" {
+#   value = aws_instance.example.id
+# }
 
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP of the web server"
-}
+# output "public_ip" {
+#   value       = aws_instance.example.public_ip
+#   description = "The public IP of the web server"
+# }
 
 output "security_group" {
   value = aws_security_group.sg_8080.id
 }
+
