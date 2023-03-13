2a514b41b0b373a2fe0fe1faf2088e8d6b02d784
gustjd887 <gustjd887@gmail.com>
2023-03-14 04:31:36 +0900

    learn-terraform-import

--- learn-terraform-import/docker.tf
+++ learn-terraform-import/docker.tf
@@ -3,18 +3,16 @@
 
 # Terraform configuration
 resource "docker_container" "web" {
-    image = "sha256:904b8cb13b932e23230836850610fa45dce9eb0650d5618c2b1487c2a4f577b8"
+    image = docker_image.nginx.latest
     name  = "hashicorp-learn"
     env = []
 
     ports {
-        external = 8080
+        external = 8081
         internal = 80
-        ip       = "::"
     }
+}
 
-    ports {
-        external = 8080
-        internal = 80
-    }
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
 }
