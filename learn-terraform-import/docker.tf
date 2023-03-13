d734ae336ba1f1d83c527afc9ca8a3190c69d7e5
gustjd887 <gustjd887@gmail.com>
2023-03-14 02:06:43 +0900

    learn-terraform-import

--- learn-terraform-import/docker.tf
+++ learn-terraform-import/docker.tf
@@ -3,5 +3,21 @@
 
 # Terraform configuration
 resource "docker_container" "web" {
-  
+    image = "sha256:904b8cb13b932e23230836850610fa45dce9eb0650d5618c2b1487c2a4f577b8"
+    name  = "hashicorp-learn"
+    env = []
+
+    ports {
+        external = 8080
+        internal = 80
+        ip       = "::"
+        protocol = "tcp"
+    }
+
+    ports {
+        external = 8080
+        internal = 80
+        ip       = "::"
+        protocol = "tcp"
+    }
 }
